7bbd3dfc90d57670792faa96a47a05f833a79cb5
Vagner Vital Pires <81259072+Vagner-Vital@users.noreply.github.com>
2021-05-24 15:22:32 -0300

    inclussão da urls, gestao de venda..

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from gestaoconstrucao import urls as urls_gestao
+
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('gestao/', include(urls_gestao)),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
